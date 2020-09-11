b361b121c00f8432e3a6d6d0e6c9d12c0de308ea
Alan Rincon <avrincon1@hotmail.com>
2020-09-11 11:41:20 +0200

    combine related formate function into one file

--- R/format_wos.R
+++ R/format_wos.R
@@ -1,4 +1,4 @@
-#' Format WebOfScience search terms
+#' Format search terms for Web of Science
 #'
 #' @param taxa A character vector of primate taxa.
 #' @param exclude A character vector of primate taxa that are higher in the taxonomic tree than those provided in the taxa argument to exclude from the search terms.
@@ -7,8 +7,29 @@
 #' @export
 #'
 #' @examples
+#' format_wos_general_terms("papio")
+#'
 #' format_wos_terms("cercopithecidae")
 #' format_wos_terms("cercopithecidae", exclude = "papio")
+#'
+#' @noRd
+
+format_wos_general_terms <- function(taxa) {
+
+  # keep columns of taxa specified in function arguments
+  ta2 <- ta[, c("term", taxa)]
+
+  # keep rows where at least one column == 1 (narrow)
+  # (indicating that search term should be used)
+  ta3 <- ta2[rowSums(sapply(ta2[ , 2:ncol(ta2), drop = FALSE], `==`, 1),
+                     na.rm = TRUE) > 0, ]
+
+  # add quotes to terms
+  ta3$term <- dQuote(ta3$term)
+
+  ta3$term
+}
+
 format_wos_terms <- function(taxa, exclude = NULL) {
 
   # convert tolower for greater flexibility in case of input
