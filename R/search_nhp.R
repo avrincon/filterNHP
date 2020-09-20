e6cfd26c6fa47059b2a1aa8e74070015ea620884
Alan Rincon <avrincon1@hotmail.com>
2020-09-20 13:27:18 +0200

    combine helper functions for search_nhp() into one file

--- R/search_nhp.R
+++ R/search_nhp.R
@@ -1,28 +1,42 @@
 #' Format non-human primate search terms for use in databases
 #'
-#' @param database A string indicating which database search terms should be formated for. Current options are "PubMed" (default), "PsycInfo" or "WebOfScience".
-#' @param taxa A character vector of primate taxa.
-#' @param exclude A character vector of primate taxa that are higher in the taxonomic tree than those provided in the taxa argument to exclude from the search terms.
+#' Function will return search terms for all taxa below the specified taxonomic
+#'  level. Search terms for humans are excluded even if they are part of that
+#'  taxonomic group.
 #'
-#' @return A string of search terms that are associated with the specified taxa, formatted for use in the specified database.
+#' @param database A string indicating which database search terms should be formatted for. Current options are "PubMed" (default), "PsycInfo" or "WebOfScience".
+#' @param taxa A character vector of primate taxa. If `taxa = NULL` (default),
+#'  function will return search terms for all non-human primates.
+#' @param exclude An optional character vector of primate taxa to exclude from
+#' the search terms. This is useful for example when you need search terms for
+#' all species of one family except one genus.
+#'
+#' @return A string of search terms that are associated with the specified taxa,
+#'  formatted for use in the specified database.
+#'
 #' @export
+#'
+#' @import data.tree
 #'
 #' @examples
 #' search_nhp(database = "PsycInfo", taxa = "papio")
-#' search_nhp(database = "PubMed", taxa = "cercopithecidae", exclude = "papio")
-#' search_nhp(database = "WebOfScience", taxa = "Platyrrhini", exclude = "Aotus")
+#' search_nhp(database = "PsycInfo", taxa = "hominidae")
+#' search_nhp(database = "PubMed", taxa = "cercopithecidae", exclude = c("papio", "macaca"))
+#' search_nhp(database = "PubMed", taxa = "platyrrhini", exclude = "aotus")
 search_nhp <-
   function(database = "PubMed",
            taxa = NULL,
            exclude = NULL) {
 
-    # convert tolower for greater flexibility in case of input
+    # convert tolower so that input is case insensitive
     if(!is.null(taxa))    taxa <- tolower(taxa)
     if(!is.null(exclude)) exclude <- tolower(exclude)
 
     # check input to function arguments are correct
-    if(!all(taxa %in% correct_taxa_inputs)){
-      xx <- setdiff(taxa, correct_taxa_inputs)
+    if(!all(taxa %in% correct_taxa_inputs) &
+       all(exclude %in% correct_taxa_inputs)){
+      xx <- c(setdiff(taxa, correct_taxa_inputs),
+              setdiff(exclude, correct_taxa_inputs))
       stop(paste("These terms are not valid taxa inputs:",
                  paste(xx, collapse = ", ")))
     }
@@ -45,3 +59,158 @@ search_nhp <-
     term
 
   }
+
+# helpers -----------------------------------------------------------------
+
+format_general_terms <- function(d, taxa) {
+  # keep columns of taxa specified in function arguments
+  d2 <- d[, c("term", taxa)]
+
+  # keep rows where at least one column == 1 (narrow)
+  # (indicating that search term should be used)
+  d3 <- d2[rowSums(sapply(d2[ , 2:ncol(d2), drop = FALSE],
+                          function(x) x == 1),
+                   na.rm = T) > 0, ]
+
+  # add quotes to terms
+  dQuote(d3$term)
+}
+
+check_higher_taxon_bracket <- function(taxa) {
+  # checks if there is only one taxonomic group below specified level
+  x <- FindNode(primate_tree, taxa)
+
+  # if there is more than one group, then this group should not be excluded so return original taxa
+  if(x$height != x$Get("totalCount")[[1]]){
+    return(taxa)
+  }
+
+  # if there is only one group, move down tree
+  while(x$height == x$Get("totalCount")[[1]]){
+    # these are the taxonomic levels that we want to exclude
+    child <- x$Get("name")
+    # move down to parent
+    x <- Navigate(x, "..")
+  }
+
+  child[child!="na"]
+}
+
+# format pubmed -----------------------------------------------------------
+
+format_pubmed_mesh <- function(taxa) {
+
+  pm2 <- pm[, c("term", taxa)]
+
+  # keep rows where at least one taxa column is not NA, and is lower level taxa
+  pm3 <- pm2[rowSums(sapply(pm2[ , 2:ncol(pm2), drop = FALSE],
+                            function(x) x %in% c("ne", "nn")),
+                     na.rm = T) > 0, ]
+
+  # if nrow is 0, there are no mesh terms, return empty object
+  if(nrow(pm3) == 0){
+    return(NULL)
+  }
+
+  # otherwise check if terms should be exploded or not
+  xx <- sapply(pm3[ , 2:ncol(pm3), drop = F], function(x) x == "ne")
+
+  # cannot do rowSums when matrix/vector has length 1, so use regular sum()
+  if(length(xx) == 1){
+    pm3$mesh <- ifelse(sum(xx, na.rm = T) > 0, "[mh]", "[mh:noexp]")
+  }
+  if(length(xx) > 1){
+    pm3$mesh <- ifelse(rowSums(xx, na.rm = T) > 0, "[mh]", "[mh:noexp]")
+  }
+
+  # quote terms
+  pm3$term <- dQuote(pm3$term)
+
+  # add extension to search term
+  paste0(pm3$term, pm3$mesh)
+}
+
+format_pubmed_terms <- function(taxa, exclude = NULL) {
+
+  mesh_terms <- format_pubmed_mesh(taxa)
+  tiab_terms <- paste0(format_general_terms(ta, taxa),
+                       "[tiab]")
+
+  if(!is.null(exclude)){
+
+    excl <- check_higher_taxon_bracket(exclude)
+
+    excl_mesh_terms <- format_pubmed_mesh(excl)
+    excl_tiab_terms <- paste0(format_general_terms(ta, excl),
+                              "[tiab]")
+
+    mesh_terms <- setdiff(mesh_terms, excl_mesh_terms)
+    tiab_terms <- setdiff(tiab_terms, excl_tiab_terms)
+  }
+
+  paste0(c(sort(mesh_terms),
+           sort(tiab_terms)), collapse = " OR ")
+
+}
+
+
+# format psycinfo ---------------------------------------------------------
+
+format_psycinfo_terms <- function(taxa, exclude = NULL) {
+
+  index_terms <- format_general_terms(pii, taxa)
+  tiab_terms <- format_general_terms(ta, taxa)
+
+  # exclude specified terms
+  if(!is.null(exclude)){
+
+    excl <- check_higher_taxon_bracket(exclude)
+
+    excl_index_terms <- format_general_terms(pii, excl)
+    excl_tiab_terms <- format_general_terms(ta, excl)
+
+    index_terms <- setdiff(index_terms, excl_index_terms)
+    tiab_terms <- setdiff(tiab_terms, excl_tiab_terms)
+  }
+
+  it <- paste0(index_terms, collapse = " OR ")
+  tt <- paste0(tiab_terms, collapse = " OR ")
+
+  # if there are no index terms, do not include it in the output
+  if (length(index_terms) == 0){
+    # message("Note: no index terms are available for the specified taxa.")
+    paste0(paste0("TX(", tt, ")"),
+           collapse = " OR ")
+
+  } else{
+    paste0(c(paste0("DE(", it, ")"),
+             paste0("TX(", tt, ")")),
+           collapse = " OR ")
+  }
+}
+
+
+# format web of science ---------------------------------------------------
+
+format_wos_terms <- function(taxa, exclude = NULL) {
+
+  general_terms <- format_general_terms(ta, taxa)
+
+  # exclude specified terms
+  if(!is.null(exclude)){
+
+    excl <- check_higher_taxon_bracket(exclude)
+    excl_general_terms <- format_general_terms(ta, excl)
+    general_terms <- setdiff(general_terms, excl_general_terms)
+  }
+
+  general_terms <- paste0(general_terms, collapse = " OR ")
+
+  wos_title_terms <- paste0("TI=(", general_terms, ")")
+  wos_abtract_terms <- paste0("AB=(", general_terms, ")")
+  wos_author_keywords_terms <- paste0("AK=(", general_terms, ")")
+
+  paste0(c(wos_title_terms,
+           wos_abtract_terms,
+           wos_author_keywords_terms), collapse = " OR ")
+}
