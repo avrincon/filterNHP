59894cacf3e50bd89188ae072f58c9ddf55d885d
Alan Rincon <avrincon1@hotmail.com>
2020-10-09 16:25:23 +0200

    add get_nhp_taxa()

---
+++ R/get_nhp_taxa.R
@@ -0,0 +1,31 @@
+#' Get taxonomic terms for non-human primates
+#'
+#'
+#' @param level Specify which level to get taxonomic terms for. Defaults to
+#'   "all". Other options are "suborder", "infraorder", "parvorder",
+#'   "superfamily", "family", "subfamily", "tribe" or "genus".
+#'
+#' @return A named list with taxonomic terms for each level
+#' @export
+#'
+#' @examples
+#' get_nhp_taxa("genus")
+get_nhp_taxa <- function(level = "all") {
+
+  level <- tolower(level)
+
+  if(!all(level %in% c("all", names(primate_taxa)))){
+    xx <- c(setdiff(level, correct_taxa_inputs))
+    stop(paste("These terms are not valid taxonomic levels:",
+               paste(xx, collapse = ", "),
+               "Please choose from:",
+               names(primate_taxa)))
+  }
+
+
+  if (level[[1]] == "all"){
+    primate_taxa
+  } else{
+    primate_taxa[level]
+  }
+}
